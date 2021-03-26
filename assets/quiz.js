cc13d953f759d1e9eeb7f49ecbb69a76c6989e33
djkaz <justbekazu@gmail.com>
2021-03-25 20:33:27 -0500

    working on it still

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,90 +1,167 @@
-var playersName = window.prompt("Please enter your name");
-alert("get ready " + playersName);
+var playersName = window.prompt("Please enter your name") 
+//{
+//if (playersName == "") {
+    alert("get ready " + playersName)
+//}
+//vars needed for points
+var playerScore = 0;
+function playerScoreTotal() {
+    
+} 
 
-function init(){
-    var count = 0, timer = setInterval(function() {
-        $("#counter").html((count++)+1);
-        if(count == 59) clearInterval(timer);
-    }, 1000);
-}
-console.log(init)
+const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
 
-function startQuiz(){
-    function init(){
-        
-    }
-    }
-//once a player hits enter first question is displayed and the timer starts
+const questionContainerElement = document.getElementById("question-container")
+let shuffledQuestions, currentQuestionIndex
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
 
-//once a player selects an answer an alert says right (true) or wrong (false) and goes to the next question
-//for a right answer the player gets 5 points and for a worng answer the player is deducted 5 points
-//a tally is kept for a final score and after the last question is answered the player is given their final score
-//var showTheScore = ?
-//keep track of the quiz answers and score
-const myQuestions = [
+const questions = [
     {
-      question: "What is a boolean?",
+        question: "What is a boolean?",
+        answers: [
+            {text: "a boomrang", correct: false },
+            {text: "a true or false question", correct: true },
+            {text: "a long line of code", correct: false},
+            {text: "bad coding", correct: false}
+        ],
+
+      },
+      {
+        question: "What does HTML stand for?",
+        answers: [
+            {text: "hyper text markup language", correct: true },
+            {text: "here then make longer", correct: false },
+            {text: "help the margin length", correct: false},
+            {text: "how to mark logs", correct: false}
+        ],
+
+      },
+      {
+        question: "How do you start a funtion?",
+        answers: [
+            {text: ";", correct: false },
+            {text: "#", correct: false },
+            {text: "+", correct: false},
+            {text: "()", correct: true}
+        ],
+
+      },
+      {
+      question: "What can you use Javascript to do?",
       answers: [
-        "a boomrang",
-        "a true or false question",
-        "a long line of code",
-        "bad coding"
-      ],
-      correctAnswer: 1
-    },
-    {
-      question: "What does HTML stand for?",
-      answers: [
-        "hyper text markup language",
-        "here then make longer",
-        "help the margin length",
-        "how to mark logs"
-      ],
-      correctAnswer: 0
-    },
-    {
-      question: "How do you start a funtion?",
-      answers: [
-        ";",
-        "#",
-        "+",
-        "()"
-      ],
-      correctAnswer: 3
-    },
-    {
-    question: "What can you use Javascript to do?",
-    answers: [
-      "make your website interactive",   
-      "you can change text and colors",
-      "you can make alerts",
-      "all of the above"
+        {text: "make your website interactive", correct: false },
+        {text: "you can change text and colors", correct: false },
+        {text: "you can make alerts", correct: false},
+        {text: "all of the above", correct: true}
     ],
-    correctAnswer: 3
-    },
-    {
-    question: "what is a for loop?",
-    answers: [
-        "it is a statment that will run if a condition is false",
-        "it is a statemtn that will run if a condition is ture",
-        "it is the if then less than statment",
-        "it will enable a user to loop audio and video",   
+
+      },
+      {
+      question: "what is a for loop?",
+      answers: [
+        {text: "it is a statment that will run if a condition is false", correct: false },
+        {text: "it is a statemtn that will run if a condition is ture", correct: false },
+        {text: "it is the if then less than statment", correct: false},
+        {text: "it will enable a user to loop audio and video", correct: true}
     ],
-    correctAnswer: 1
-    }]
 
-const quizContainer = document.getElementById('quiz');
+ }]
 
-const resultsContainer = document.getElementById('results');
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener("click", () => {
+    setNextQuestion()
 
-const submitButton = document.getElementById('submit');
+})
 
-var theQuestions = 0
-
-
-function answerQuestions() {
-    const aQuestion = myQuestions[theQuestions];
-    var questionOne = aQuestion.question
-        console.log(questionOne)
+function startGame() {
+    console.log("starting the game")
+    startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    startNextQuestion()
 }
 
+const startingTimer = 1;
+let time = startingTimer * 60;
+
+const countdownEl = document.getElementById("countdown");
+
+setInterval(updateCountdown, 1000);
+    
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+
+    //if seconds === 0, 
+}
+ /*if (num <= 1) {
+     return 1:
+ }
+
+ var r factor(60)
+ */
+
+function startNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+    if (answer.correct) {
+        button.dateset.correct = answer.correct
+        playerScore = playerScore + 5;
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.datatset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        playersName()
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    }
+        else {
+            element.classList.add("wrong")
+        }
+    }
+
+
+function clearStatusClass(element) {
+            element.classList.remove("correct")
+            element.classList.remove("wrong")
+        }
+
+
+
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
